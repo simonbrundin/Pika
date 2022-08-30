0f8233bb753670db1706789ab8d553e15a7fe05b
Simon Brundin <simonbrundin@gmail.com>
2022-08-30 17:51:06 +0200

    gql

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from "nuxt";
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["nuxt-graphql-client"],
+  runtimeConfig: {
+    public: {
+      "graphql-client": {
+        clients: {
+          default: {
+            host: "https://mooni.hasura.app/v1/graphql", // overwritten by process.env.GQL_SHOPIFY_HOST
+            token: {
+              name: "x-hasura-admin-secret/x-hasura-access-key", // overwritten by process.env.GQL_SHOPIFY_TOKEN_NAME
+            },
+          },
+        },
+      },
+    },
+  },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,6 @@
   <div><Input /></div>
 </template>
 
-<script setup></script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
