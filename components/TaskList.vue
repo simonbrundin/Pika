088d9193f01db08edc6b1fdb2af30a99287b363f
Simon Brundin <simonbrundin@gmail.com>
2022-09-04 14:49:29 +0200

    added pinia values

--- components/TaskList.vue
+++ components/TaskList.vue
@@ -2,12 +2,15 @@
   <div>
     <div v-for="(task, index) in data.task" :key="index">
       {{ task.title }}
+      {{ store.test }}
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useStore } from "@/stores/store";
 const { data } = await useAsyncGql("AllTasks");
+const store = useStore();
 console.log("Tasks hämtade");
 </script>
 
