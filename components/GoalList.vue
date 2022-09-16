6f3386e3248e7f88d449b02d0441e289a82e1568
Simon Brundin <simonbrundin@gmail.com>
2022-09-16 11:47:15 +0200

    Added edit name

--- components/GoalList.vue
+++ components/GoalList.vue
@@ -6,14 +6,21 @@
       class="bg-slate-50 shadow-xl px-6 py-4 rounded-full flex justify-between m-1"
       @click="setGoalInFocus(goal.id)"
     >
-      <div>
-        {{ goal.title }}
+      <div class="flex justify-start">
+        <div @click="goal.done = true" v-if="false">☉</div>
+        <div :class="{ done: goal.done }">
+          {{ goal.title }}
+        </div>
       </div>
-      <div
-        @click="deleteGoal(goal.id)"
-        v-if="store.children(goal.id).length === 0"
-      >
-        x
+      <div class="flex justify-end">
+        <div
+          @click="deleteGoal(goal.id)"
+          class="mr-4"
+          v-if="store.children(goal.id).length === 0"
+        >
+          x
+        </div>
+        <div @click="editGoal(goal.id)">⦂</div>
       </div>
     </div>
   </div>
@@ -22,6 +29,16 @@
 <script setup lang="ts">
 import { useStore } from "@/stores/store";
 const store = useStore();
+const editGoal = (id, title) => {
+  setGoalInFocus(store.goalInFocus);
+  const newTitle = prompt("Målnamn", title);
+  const index = store.goals.findIndex((goal) => goal.id === id);
+  store.goals[index].title = newTitle;
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.done {
+  color: gray;
+}
+</style>
