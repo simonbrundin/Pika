2088ab4fbc5f080202596a5d3334e27dd558408b
Simon Brundin <simonbrundin@gmail.com>
2022-09-16 10:44:22 +0200

    only show remove on bottom layer

--- components/GoalList.vue
+++ components/GoalList.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="flex flex-col">
     <div
-      v-for="(goal, index) in store.children"
+      v-for="(goal, index) in store.children(store.goalInFocus)"
       :key="index"
       class="bg-slate-50 shadow-xl px-6 py-4 rounded-full flex justify-between m-1"
+      @click="setGoalInFocus(goal.id)"
     >
-      <div @click="setGoalInFocus(goal.id)">
+      <div>
         {{ goal.title }}
       </div>
-      <div @click="deleteGoal(goal.id)">x</div>
+      <div
+        @click="deleteGoal(goal.id)"
+        v-if="store.children(goal.id).length === 0"
+      >
+        x
+      </div>
     </div>
   </div>
 </template>
@@ -16,7 +22,6 @@
 <script setup lang="ts">
 import { useStore } from "@/stores/store";
 const store = useStore();
-updateAllGoals();
 </script>
 
 <style scoped></style>
